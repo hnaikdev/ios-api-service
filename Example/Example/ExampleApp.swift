b553f624e32bb17a0830bea4529bbad48c26ed54
Hiral Naik <hrlnaik@gmail.com>
2025-07-25 12:59:48 -0700

    created sample app project

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Hiral Naik on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
